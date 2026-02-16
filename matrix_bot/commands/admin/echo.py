8f299b2cab093836e3a0716c4ea13ac74ade964a
Voxy <development@voxvoltera.com>
2026-02-16 13:47:28 +0100

    Moans in malte

--- matrix_bot/commands/admin/echo.py
+++ matrix_bot/commands/admin/echo.py
@@ -5,12 +5,12 @@ def register(bot):
     async def echo(room, event, match):
         print(f'event {event}')
 
-            message = match.args()
-            if len(message) == 0:
-                message = 'no message provided'
-            message = " ".join(message)
-            await bot.api.send_text_message(
-                room.room_id,
-                f"{message}"
-            )
+        message = match.args()
+        if len(message) == 0:
+            message = 'no message provided'
+        message = " ".join(message)
+        await bot.api.send_text_message(
+            room.room_id,
+            f"{message}"
+        )
         return
